495169c0dd84d5f6fbc7e1ed8cc183437d52b088
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2022-06-01 05:42:34 +0000

    add pyenv install and git setup

---
+++ pyenv_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+apt install -y \
+	build-essential \
+	openssl \
+	libffi-dev \
+	libssl-dev \
+	zlib1g-dev \
+	liblzma-dev \
+	libbz2-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	libopencv-dev \
+	tk-dev \
+	git
+
+exit
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+cd $HOME/.pyenv
+git checkout v2.0.3
+
+# .bashrcの更新
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+source ~/.bashrc
