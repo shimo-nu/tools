07f8f22bbb69359b104158dabd67d46735840eb8
shimo-nu <shimo@ucl.nuee.nagoya-u.ac.jp>
2022-06-06 16:27:31 +0900

    add pyenv install scripts

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
+if [ `whoami` = "root" ]; then
+	apt install -y \
+		build-essential \
+		openssl \
+		libffi-dev \
+		libssl-dev \
+		zlib1g-dev \
+		liblzma-dev \
+		libbz2-dev \
+		libreadline-dev \
+		libsqlite3-dev \
+		libopencv-dev \
+		tk-dev \
+		git
 
-apt install -y \
-	build-essential \
-	openssl \
-	libffi-dev \
-	libssl-dev \
-	zlib1g-dev \
-	liblzma-dev \
-	libbz2-dev \
-	libreadline-dev \
-	libsqlite3-dev \
-	libopencv-dev \
-	tk-dev \
-	git
-
-exit
-
+fi
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
 cd $HOME/.pyenv
 git checkout v2.0.3
 
-# .bashrcの更新
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
